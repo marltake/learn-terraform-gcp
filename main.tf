4db49a4c270d049c915fbfe0f40f3afef210fc85
Otake Shigenori <otake.shigenori@gmail.com>
2021-08-09 13:38:21 +0900

    add compute instance
    https://learn.hashicorp.com/tutorials/terraform/google-cloud-platform-change

--- main.tf
+++ main.tf
@@ -19,3 +19,20 @@ resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
 }
 
+resource "google_compute_instance" "vm_instance" {
+  name         = "frontend"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
