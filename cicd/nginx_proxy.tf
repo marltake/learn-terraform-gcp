bb5102bf45259017b366abc6d99dfacc2f374150
Otake Shigenori <otake.shigenori@gmail.com>
2021-08-23 07:51:49 +0900

    recreate vm

---
+++ cicd/nginx_proxy.tf
@@ -0,0 +1,47 @@
+resource "google_compute_network" "vpc_network" {
+  name = "cicd-network"
+}
+
+resource "google_compute_instance" "nginx_instance" {
+  name         = "reverse-proxy"
+  machine_type = "e2-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "develop" {
+  name    = "ssh-firewall"
+  network = google_compute_network.vpc_network.name
+
+  direction = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  target_tags   = ["dev"]
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "frontend" {
+  name    = "http-firewall"
+  network = google_compute_network.vpc_network.name
+
+  direction = "INGRESS"
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+  target_tags   = ["web"]
+  source_ranges = ["0.0.0.0/0"]
+}
