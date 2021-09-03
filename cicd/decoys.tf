e4d8495f82963416728714c988ac9c46e4281590
Otake Shigenori <otake.shigenori@gmail.com>
2021-09-04 00:19:50 +0900

    add decoy

---
+++ cicd/decoys.tf
@@ -0,0 +1,20 @@
+resource "google_compute_instance" "decoy" {
+  name         = "decoy"
+  machine_type = "f1-micro"
+  tags         = ["dev"]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"
+    }
+  }
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+  scheduling {
+    preemptible         = true
+    automatic_restart   = false
+    on_host_maintenance = "TERMINATE"
+  }
+}
